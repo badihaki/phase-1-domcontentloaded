2cbb7825010ad57fe0e7f0e1d92a80ae88cc2bea
Badi-Haki Martin <jojackblack@gmail.com>
2022-03-15 19:01:36 -0400

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", loadDocument)
+
+function loadDocument(){
+    document.getElementById('text').textContent = 'This is really cool!';
+}
